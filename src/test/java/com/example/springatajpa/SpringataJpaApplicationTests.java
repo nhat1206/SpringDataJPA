90996cf39aaf92545afc52184b2d3e0173c8a55a
nhat1206 <nhnhat1206@gmail.com>
2021-09-14 20:36:41 +0700

    create and save and delete Repository Branch

--- src/test/java/com/example/springatajpa/SpringataJpaApplicationTests.java
+++ src/test/java/com/example/springatajpa/SpringataJpaApplicationTests.java
@@ -1,13 +1,51 @@
 package com.example.springatajpa;
 
+import entity.Employee;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import repository.EmploeeRepository;
+
+import java.util.List;
 
 @SpringBootTest
 class SpringataJpaApplicationTests {
+
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    private EmploeeRepository emploeeRepository;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void findAll(){
+        List<Employee> employees = emploeeRepository.findAll();
+        employees.forEach(System.out::println);
+    }
+
+    @Test
+    void createEmployee(){
+        Employee employee = new Employee();
+        employee.setFirstName("Han");
+        employee.setLastName("Solo");
+        employee.setJob("Teacher");
+        emploeeRepository.save(employee);
+    }
+
+    @Test
+    void updateEmployee(){
+        Employee employee = new Employee();
+        employee.setId(3);
+        employee.setFirstName("Harry");
+        employee.setLastName("Style");
+        employee.setJob("Celebrity");
+        emploeeRepository.save(employee);
+    }
+
+    @Test
+    void deleteEmployee(){
+        emploeeRepository.deleteById(2);
+    }
 }
